e63b9d36f0d039deee3ec514bfee9a37c9b48bb9
Mateo <mateo.allegre@etu.uca.fr>
2023-07-03 16:19:29 +0200

    debut de la fonction jeu pour un robot

--- jeu.c
+++ jeu.c
@@ -1,18 +1,41 @@
 #include "jeu.h"
 
 /* renvoie le score. Simule le jeu et run une de ces instance */
-int Jeu(bot){
+int Jeu(bot robot){
+    bool fin = false;
+    int i;
+    int deplacement;
+    int situation[4];
+    
+    // initialisation du jeu
+    jeu j;
+    for (int y = 0; y < NB_LIGNES; y++) {
+	for (int x = 0; x < NB_COLONNES; x++) {
+	    j.grille[y][x] = 0;
+	}
+    }
+    j.chasseur = rand() % NB_COLONNES;
+    j.proie = rand() % NB_COLONNES;
 
-  /* SDL */
-  /* iterJeu(grille, proie, chasseur, deplacement) */
-
-  /* for (i=0; i < MAX_ITER; i++) { */
-/* iterJeu(grille, proie, chasseur, deplacement) */
-/* } */
-
+    while (i > MAX_ITER && !fin) {
+	getSituationFromJeu(j, situation);
+	deplacement = deplacementFromBot(robot, situation);
+	fin = iterJeu(j, deplacement);
+	i++;
+    }
+    return i;
 }
 
-bool iterJeu(int grille[], int * proie, int * chasseur, int deplacement){
+void getSituationFromJeu(jeu j, int situation[4]){
+    // modifier la situation en fonction du jeu
+}
+
+int deplacementFromBot(bot robot, int situation[4]){
+    // renvoyer ce que fait le bot dans le cas donne
+    return DIRMILIEU;
+}
+
+bool iterJeu(jeu j, int deplacement){
 
   /* SDL */
   /* iterJeu(grille, proie, chasseur, deplacement) */
@@ -21,6 +44,5 @@ bool iterJeu(int grille[], int * proie, int * chasseur, int deplacement){
 /* iterJeu(grille, proie, chasseur, deplacement) */
 /* } */
 
-  /* test */
-
+    // appel à decalerGrille qui genere une nouvelle ligne et decale les autres
 }
--- jeu.h
+++ jeu.h
@@ -5,20 +5,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define PROCHE 1
-#define MOYEN 2
-#define LOIN 3
-#define AUCUN 4
+// Valeurs pour les distances des plus proches obstacles
+#define PROCHE 1 // immediatement sur la ligne suivante
+#define MOYEN 2 // entre 2 et 4 lignes plus loin
+#define LOIN 3 // plus de 4 lignes
+#define AUCUN 4 // pas d'obstacle
 
-#define EGAUCHE 1
-#define GAUCHE 2
-#define CENTRE 3
-#define DROITE 4
-#define EDROITE 5
+// Valeurs pour la position de la proie
+#define EGAUCHE 1 // à gauche mais pas directement
+#define GAUCHE 2 // immédiatement à gauche
+#define CENTRE 3 // devant
+#define DROITE 4 // immédiatement à droite
+#define EDROITE 5 // à droite mais pas directement
 
+// types regle et bot
+/* Une regle contient six entiers :
+   - la distance de l'obstacle le plus bas sur la colonne à gauche de la proie
+   - la distance de l'obstacle le plus bas sur la colonne de la proie
+   - la distance de l'obstacle le plus bas sur la colonne à droite de la proie
+   - la position de la proie par rapport au chasseur
+   - l'action du bot (dirgauche, dirmilieu, dirdroite)
+   - la priorite entre 1 et 5 (5 : plus important)
+*/
 typedef int regle[6];
 typedef regle bot[10];
 
+// Directions possibles de deplacement (outputs d'une regle)
 #define DIRGAUCHE -1
 #define DIRMILIEU 0
 #define DIRDROITE 1
+
+// prototypes
+bool iterJeu(jeu j, int deplacement);
+void getSituationFromJeu(jeu j, int situation[4]);
+int deplacementFromBot(bot robot, int situation[4]);

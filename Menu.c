20f8d1a89aab80596cf5b012f8146dc974a3672f
Malik Makkes <93526737+MalikSploit@users.noreply.github.com>
2023-07-03 14:22:38 +0200

    Update Menu.c
    Ajout des headers

---
+++ Menu.c
@@ -0,0 +1,30 @@
+#ifndef PROJET_ZZ1_MENU_H
+#define PROJET_ZZ1_MENU_H
+
+#include "Constantes.h"
+#include "SDL_Initialisation.h"
+
+typedef struct Button
+{
+    SDL_Rect rect;
+    SDL_Color color;
+    char* text;
+    TTF_Font* font;
+    SDL_Texture* texture;
+} Button;
+
+
+typedef struct
+{
+    char name[50];
+    int score;
+} PlayerScore;
+
+
+void drawButton(SDL_Renderer* renderer, Button* button);
+void drawText(SDL_Renderer* renderer, TTF_Font* font, char* text, SDL_Color color, int x, int y);
+SDL_Texture* loadTexture(SDL_Renderer* renderer, char* filepath);
+void displayHelp();
+void displayHighScore();
+
+#endif //PROJET_ZZ1_MENU_H

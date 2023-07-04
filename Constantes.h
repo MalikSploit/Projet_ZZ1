06d5079b2f7d08cd2a29f303bf43acfaab175cf5
MalikSploit <maksucce@gmail.com>
2023-07-05 01:36:11 +0200

    Initialisation des obstacles dans initJeu

--- Constantes.h
+++ Constantes.h
@@ -10,13 +10,13 @@
 
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
-
 #define NB_LIGNES 8
 #define NB_COLONNES 8
-
 #define MAX_ITER 100
-
 #define NOMBRE_SPRITE 8
+#define TAILLE_CELLULE_LARGEUR 361
+#define TAILLE_CELLULE_LONGUEUR 243
+#define MAX_OBSTACLES 6
 
 typedef struct {
     int grille[NB_LIGNES][NB_COLONNES]; // 0 : case vide, 1 : obstacle
